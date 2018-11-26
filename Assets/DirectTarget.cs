05cc29de4ade4425bf6b415105da65d954d64920
Shina Mashiro <shinamashiro@ShinadeMBP.lan>
2018-11-26 16:06:56 +0800

    Initial commit

---
+++ Assets/DirectTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectTarget : MonoBehaviour
+{
+
+	public GameObject Target;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<Renderer>().material.SetVector("_TargetPosition",Target.transform.position);
+	}
+}
---
+++ Assets/move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	float h=0, v=0;
+	// Update is called once per frame
+	void Update ()
+	{	
+		h -= Input.GetAxis("Mouse X");
+		v += Input.GetAxis("Mouse Y");
+		Camera.main.transform.eulerAngles=new Vector3(-v,-h,0);
+
+		
+		
+		transform.GetComponent<Rigidbody>().AddForce(Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0)*new Vector3(Input.GetAxis("Horizontal") * 20, 0, Input.GetAxis("Vertical") * 20));	
+	}
+}
